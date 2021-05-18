3c32690886a888faaf544bff2efee88b4f4981ec
DGorgula <gil@dankner.org>
2021-05-18 04:29:25 +0300

    plus style fixed

--- src/components/ChatRoomEntrance.js
+++ src/components/ChatRoomEntrance.js
@@ -118,8 +118,10 @@ function ChatRoomEntrance({ chatId, dbUser }) {
                     </div>
                 );
             })}
-                <div id="add-room" onClick={openNewRoomForm}>
-                    +
+                <div className="room-block plus" onClick={openNewRoomForm}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" id="plus" viewBox="0 0 16 16">
+                        <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+                    </svg>
                 </div>
             </div>
             ) : <p>{"Loading..."}</p>}
@@ -134,9 +136,11 @@ function ChatRoomEntrance({ chatId, dbUser }) {
                     </div>
                 );
             })}
-                <div id="add-room" onClick={openNewForeignRoomForm}>
-                    +
-</div>
+                <div className="room-block plus" onClick={openNewForeignRoomForm}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" id="plus" viewBox="0 0 16 16">
+                        <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+                    </svg>
+                </div>
             </div>
             ) : <p>{"Loading..."}</p>}
 
